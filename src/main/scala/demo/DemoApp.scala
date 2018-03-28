3cfe29e593a0eb0ad0ba39976502cff8746b28cd
daoudi el hassan <elha_da@hotmail.com>
2018-03-28 14:28:56 +0200

    side-effects :tada:

--- src/main/scala/demo/DemoApp.scala
+++ src/main/scala/demo/DemoApp.scala
@@ -11,9 +11,20 @@ object DemoApp extends App {
   val program: IO[Either[Error, Unit]] =
     convert(File.temp / "demofile", File.temp / "result")
 
-  program.unsafeRunSync() match {
-    case Left(error) => println(s"error: $error")
-    case Right(_) => println("successfully written")
+  //  program.unsafeRunSync() match {
+  //    case Left(error) => println(s"error: $error")
+  //    case Right(_) => println("successfully written")
+  //  }
+
+  def handle(e: Either[Error, Unit]) = e match {
+    case Left(error) => IO {
+      println(s"error: $error")
+    }
+    case Right(value) => IO {
+      println("successfully written")
+    }
   }
+
+  program.flatMap(e => handle(e)).unsafeToFuture() //unsafeRunSync()
 
 }
